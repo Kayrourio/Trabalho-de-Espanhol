fbd9238b4380f055f2440818d2183e01e9ec0b04
Kayrourio <78915485+Kayrourio@users.noreply.github.com>
2021-09-23 09:58:21 -0300

    Add files via upload

---
+++ manual.js
@@ -0,0 +1,23 @@
+var abrir_manual = document.querySelector(".c");
+var abrir_esp = document.querySelector(".a");
+var abrir_info = document.querySelector(".b");
+var menu_inicial = document.querySelector(".d")
+
+
+abrir_esp.addEventListener("click", ir_pag_esp);
+abrir_info.addEventListener("click", ir_pag_info);
+menu_inicial.addEventListener("click", voltar_menu)
+
+
+function ir_pag_esp(){
+    console.log("abriu");
+    window.location.href = "manuales.html";
+}
+function ir_pag_info(){
+    console.log("abriu");
+    window.location.href = "info.html";
+}
+function voltar_menu(){
+    console.log("abriu");
+    window.location.href = "index.html";
+}
